d1ffa04a4eaf92700894c3b1d0378a6dcabe87c3
adupoku71 <davidpoku71@gmail.com>
2025-08-19 23:51:49 +0000

    styled pages with tailwind and made some changes to forms

--- todo/forms.py
+++ todo/forms.py
@@ -2,14 +2,16 @@ from django import forms
 from .models import Task
 
 
+
+class AddTaskForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = ["title"]
+        
+        
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = "__all__"
         
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        # Make fields conditionally required
-        self.fields['title'].required = False
-        self.fields['description'].required = False
         
